fae811a7dba0ea04cc7c0a04f64ad5327e761d10
chihpingkuo <chihpingkuo@msn.com>
2024-05-17 10:46:18 +0800

    reactive d3 chart with alpine.js

--- app/static/app.js
+++ app/static/app.js
@@ -1,4 +1,4 @@
-function App() {
+async function App() {
   const width = 640;
   const height = 400;
   const marginTop = 20;
@@ -6,10 +6,17 @@ function App() {
   const marginBottom = 30;
   const marginLeft = 40;
 
+  let store = Alpine.reactive({ data: [] });
+
+  setInterval(async () => {
+    store.data = await d3.json("/bt");
+    console.log(store.data);
+  }, 2000);
+
   // Declare the x (horizontal position) scale.
   const x = d3
-    .scaleUtc()
-    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+    .scaleLinear()
+    .domain([0, 900])
     .range([marginLeft, width - marginRight]);
 
   // Declare the y (vertical position) scale.
@@ -17,6 +24,12 @@ function App() {
     .scaleLinear()
     .domain([0, 100])
     .range([height - marginBottom, marginTop]);
+
+  // Declare the line generator.
+  const line = d3
+    .line()
+    .x((d) => x(d.t))
+    .y((d) => y(d.v));
 
   // Create the SVG container.
   const svg = d3.create("svg").attr("width", width).attr("height", height);
@@ -32,6 +45,19 @@ function App() {
     .append("g")
     .attr("transform", `translate(${marginLeft},0)`)
     .call(d3.axisLeft(y));
+
+  // Append a path for the line.
+  svg
+    .append("path")
+    .attr("id", "id1")
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", line(store.data));
+
+  Alpine.effect(() => {
+    d3.select("#id1").attr("d", line(store.data));
+  });
 
   // Append the SVG element.
   const container = document.getElementById("container");

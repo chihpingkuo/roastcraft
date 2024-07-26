85d6096a6aa51f6cb6d8d93cd4b5b08c0f8a123d
chihpingkuo <chihpingkuo@msn.com>
2024-07-26 12:30:21 +0800

    clean up app.js

--- app/static/index.js
+++ app/static/index.js
@@ -22,61 +22,60 @@ const app = createApp({
 
         let channels_nodes = [];    
 
+        let roast_events_node;
+
+        const width = 860;
+        const height = 550;
+        const marginTop = 20;
+        const marginRight = 20;
+        const marginBottom = 30;
+        const marginLeft = 40;
+        
+        // Declare the x (horizontal position) scale.
+        const xScale = d3
+        .scaleLinear()
+        .domain([-60, 780])
+        .range([marginLeft, width - marginRight]);
+
+        // Declare the y (vertical position) scale.
+        const yScale = d3
+        .scaleLinear()
+        .domain([80, 380])
+        .range([height - marginBottom, marginTop]);
+
+        // Declare the y (vertical position) scale.
+        const yScaleROR = d3
+        .scaleLinear()
+        .domain([0, 24])
+        .range([height - marginBottom, marginTop]);
+
+        // Declare the line generator.
+        const line = d3.line()
+        .x((p) => xScale(p.t))
+        .y((p) => yScale(p.v));
+
+        const lineROR = d3.line()
+        .x((p) => xScale(p.t))
+        .y((p) => yScaleROR(p.v));
+
         onMounted(() => {    
         
-            const width = 860;
-            const height = 550;
-            const marginTop = 20;
-            const marginRight = 20;
-            const marginBottom = 30;
-            const marginLeft = 40;
-            
-            // Declare the x (horizontal position) scale.
-            const xScale = d3
-            .scaleLinear()
-            .domain([-60, 780])
-            .range([marginLeft, width - marginRight]);
-
-            // Declare the y (vertical position) scale.
-            const yScale = d3
-            .scaleLinear()
-            .domain([80, 380])
-            .range([height - marginBottom, marginTop]);
-
-            // Declare the y (vertical position) scale.
-            const yScaleROR = d3
-            .scaleLinear()
-            .domain([0, 24])
-            .range([height - marginBottom, marginTop]);
-
-            // Declare the line generator.
-            const line = d3.line()
-            .x((p) => xScale(p.t))
-            .y((p) => yScale(p.v));
-
-            const lineROR = d3.line()
-            .x((p) => xScale(p.t))
-            .y((p) => yScaleROR(p.v));
-
             // Create the SVG container.
             const svg = d3.select("#main_chart")
                 .attr("width", width)
                 .attr("height", height);
             
             // Add the x-axis.
-            svg
-                .append("g")
+            svg.append("g")
                 .attr("transform", `translate(0,${height - marginBottom})`)
                 .call(d3.axisBottom(xScale));
 
             // Add the y-axis.
-            svg
-                .append("g")
+            svg.append("g")
                 .attr("transform", `translate(${marginLeft},0)`)
                 .call(d3.axisLeft(yScale));
 
-            svg
-                .append("g")
+            svg.append("g")
                 .attr("transform", `translate(${width - marginRight},0)`)
                 .call(d3.axisRight(yScaleROR));      
 
@@ -92,14 +91,29 @@ const app = createApp({
                 )
             });
         
-            let roast_events_node = svg.append("g")
+            svg.append("path")
+                .attr("id", "BT_ROR")
+                .attr("fill", "none")
+                .attr("stroke", "#2E8B57")
+                .attr("stroke-width", 1)
+                .attr("d", lineROR(store.channels[0].ror));
+
+            svg.append("path")
+                .attr("id", "BT_ROR_SMOOTH")
+                .attr("fill", "none")
+                .attr("stroke", "#0000FF")
+                .attr("stroke-width", 2)
+                .attr("d", lineROR(store.channels[0].ror_smoothed));
+
+            roast_events_node = svg.append("g")
             
             watchEffect(() => {
                 for(let i = 0 ; i < store.channels.length ; i++) {
                   channels_nodes[i].attr("d", line(store.channels[i].data));  
                 }
             
-               
+                d3.select("#" + "BT_ROR").attr("d", lineROR(store.channels[0].ror));
+                d3.select("#" + "BT_ROR_SMOOTH").attr("d", lineROR(store.channels[0].ror_smoothed));
             });
             
         })
@@ -117,14 +131,48 @@ const app = createApp({
         socket.on('update_timer', 
             (t) => { 
 
-                console.log(t);
-
                 timer.value = 
                 Math.floor(Math.round(t) / 60).toString() +
                         ':' +
                         (Math.round(t) % 60).toString().padStart(2, '0');
-                console.log(timer.value);
-            });
+               
+        });
+
+        socket.on("roast_events", (roast_events) => {
+            console.log(roast_events);
+            store.roast_events = roast_events;
+        
+            roast_events_node.selectAll("*").remove();
+        
+            for (const[key, idx] of Object.entries(roast_events)) {
+                if (idx != 0){
+                let x = xScale(store.channels[0].data[idx].t)
+                let y = yScale(store.channels[0].data[idx].v)
+        
+                roast_events_node.append("line")
+                    .attr("stroke", "black")
+                    .attr("stroke-width", 1)
+                    .attr("x1", x + 2)
+                    .attr("y1", y + 2)
+                    .attr("x2", x + 10)
+                    .attr("y2", y + 10)
+        
+                roast_events_node.append("circle")
+                    .attr("cx", x)
+                    .attr("cy", y)
+                    .attr("r", 2)
+                
+                roast_events_node.append("text")
+                    .attr("alignment-baseline", "hanging" )
+                    .attr("font-size", "small" )
+                    .attr("x", x + 10)
+                    .attr("y", y + 10)
+                    .text(key)
+                }
+        
+            }
+            
+        });
 
         return {
             timer,
